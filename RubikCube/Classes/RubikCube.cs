3d17f81566e2f0bee76b8850c9bb2d27b1cc4021
zhaogl1991 <931420039@qq.com>
2018-06-01 13:32:22 +0800

    Initial commit

---
+++ RubikCube/Classes/RubikCube.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RubikCube.Classes
+{
+    enum Direction
+    {
+        Ahead,
+        Back,
+        Left,
+        Right,
+        Up,
+        Down
+    }
+
+    enum Alis
+    {
+        X,
+        Y,
+        Z
+    }
+
+    class Orientation
+    {
+        Alis alis;
+        bool direct;
+    }
+
+    class position
+    {
+
+    }
+
+    class position1:position
+    {
+        Orientation orient1;
+    }
+
+    class position2 : position
+    {
+        Orientation orient1;
+        Orientation orient2;
+    }
+
+    class position3 : position
+    {
+        Orientation orient1;
+        Orientation orient2;
+        Orientation orient3;
+    }
+
+
+    class Face
+    {
+        /// <summary>
+        /// 包含的方块
+        /// </summary>
+        List<Cube> Cubes;
+
+        /// <summary>
+        /// 位置
+        /// </summary>
+        position1 Location;
+        /// <summary>
+        /// 顺时针转动
+        /// </summary>
+        public void RotateClockwise()
+        {
+
+        }
+        /// <summary>
+        /// 逆时针转动
+        /// </summary>
+        public void RotateAntiClockwise()
+        {
+
+        }
+    }
+    class Cube
+    {
+        int direction;
+    }
+    class Cube1:Cube
+    {
+        position1 PositionOriginal;
+        position1 PositionNow;
+        position1 Direction;
+    }
+    class Cube2 : Cube
+    {
+
+    }
+    class Cube3 : Cube
+    {
+
+    }
+    class RubikCube
+    {
+        
+    }
+}
