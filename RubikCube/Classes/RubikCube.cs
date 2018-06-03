9cde513fd405dab84877a944b59cbb43c52359c3
赵国梁 <931420039@qq.com>
2018-06-03 21:03:34 +0800

    写了一点两面方块的代码
    20180603

--- RubikCube/Classes/RubikCube.cs
+++ RubikCube/Classes/RubikCube.cs
@@ -23,10 +23,72 @@ namespace RubikCube.Classes
         Z
     }
 
+    /// <summary>
+    /// 方向
+    /// 分为X,Y,Z三条轴
+    /// 和
+    /// 正，负两个方向
+    /// </summary>
     class Orientation
     {
-        Alis alis;
-        bool direct;
+        public Alis alis;
+        public bool direct;
+        public Orientation(Alis a,bool di)
+        {
+            alis = a;
+            direct = di;
+        }
+
+        public object Clone()
+        {
+            return new Orientation(this.alis, this.direct);
+        }
+        public static bool operator ==(Orientation or1,Orientation or2)
+        {
+            if(or1.alis==or2.alis&&or1.direct==or2.direct)
+            {
+                return true;
+            }
+            return false;
+        }
+        public static bool operator !=(Orientation or1, Orientation or2)
+        {
+            if (or1.alis != or2.alis || or1.direct != or2.direct)
+            {
+                return true;
+            }
+            return false;
+        }
+        public override bool Equals(object obj)
+        {
+            if(obj==null)
+            {
+                return false;
+            }
+            Orientation or2 = obj as Orientation;
+            if((System.Object)or2 == null)
+            {
+                return false;
+            }
+            if (this.alis == or2.alis && this.direct == or2.direct)
+                return true;
+            else
+                return false;
+        }
+
+        /// <summary>
+        /// 判断是否是当前方向的反方向
+        /// </summary>
+        /// <param name="or2">被判断方向</param>
+        /// <returns></returns>
+        public bool isOppositeDrectionOf(Orientation or2)
+        {
+            if(this.alis == or2.alis&&(!this.direct)==or2.direct)
+            {
+                return true;
+            }
+            return false;
+        }
     }
 
     class position
@@ -36,20 +98,20 @@ namespace RubikCube.Classes
 
     class position1:position
     {
-        Orientation orient1;
+        public Orientation orient1;
     }
 
     class position2 : position
     {
-        Orientation orient1;
-        Orientation orient2;
+        public Orientation orient1;
+        public Orientation orient2;
     }
 
     class position3 : position
     {
-        Orientation orient1;
-        Orientation orient2;
-        Orientation orient3;
+        public Orientation orient1;
+        public Orientation orient2;
+        public Orientation orient3;
     }
 
 
@@ -81,7 +143,9 @@ namespace RubikCube.Classes
     }
     class Cube
     {
-        int direction;
+        public virtual bool MoveTo_OneSkip(position p){
+            return true;
+        }
     }
     class Cube1:Cube
     {
@@ -91,11 +155,71 @@ namespace RubikCube.Classes
     }
     class Cube2 : Cube
     {
+        position2 PositionOriginal;
+        position2 PositionNow;
+        position2 Direction;
 
+        public override bool MoveTo_OneSkip(position p)
+        {
+            if (p is Classes.position2)
+            {
+                position2 pTo = p as position2;
+                Orientation stand = null;
+                Orientation left1 = null;
+                Orientation left2 = null;
+
+                if(PositionNow.orient1==pTo.orient1)
+                {
+                    stand = PositionNow.orient1.Clone() as Orientation;
+                    left1 = PositionNow.orient2.Clone() as Orientation;
+                    left2 = pTo.orient2.Clone() as Orientation;
+                }
+                else if (PositionNow.orient1 == pTo.orient2)
+                {
+                    stand = PositionNow.orient1.Clone() as Orientation;
+                    left1 = PositionNow.orient2.Clone() as Orientation;
+                    left2 = pTo.orient1.Clone() as Orientation;
+                }
+                else if (PositionNow.orient2 == pTo.orient1)
+                {
+                    stand = PositionNow.orient2.Clone() as Orientation;
+                    left1 = PositionNow.orient1.Clone() as Orientation;
+                    left2 = pTo.orient2.Clone() as Orientation;
+                }
+                else if (PositionNow.orient2 == pTo.orient2)
+                {
+                    stand = PositionNow.orient2.Clone() as Orientation;
+                    left1 = PositionNow.orient1.Clone() as Orientation;
+                    left2 = pTo.orient1.Clone() as Orientation;
+                }
+                if(left1==null||left2==null)
+                {
+                    return false;
+                }
+                else
+                {
+                    if(left1==left2||left1.isOppositeDrectionOf(left2))
+                    {
+                        return false;
+                    }
+                    else
+                    {
+
+                        return true;
+                    }
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
     class Cube3 : Cube
     {
-
+        position3 PositionOriginal;
+        position3 PositionNow;
+        position3 Direction;
     }
     class RubikCube
     {
--- RubikCube/Form1.cs
+++ RubikCube/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RubikCube.Classes;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,12 @@ namespace RubikCube
         public Form1()
         {
             InitializeComponent();
+
+            TestObject a = new TestObject();
+            a.data = 1;
+            TestObject b = a;
+            a.data = 2;
+            MessageBox.Show(string.Format("a={0},b={1}",a.data,b.data));
         }
     }
 }
